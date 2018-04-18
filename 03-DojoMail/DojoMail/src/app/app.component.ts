68139c0cc5250d058ce55e81770619c7eaa4edab
Chia T <tham_oh@hotmail.com>
2018-04-18 00:28:45 -0500

    add angular, dojo mail

--- 03-DojoMail/DojoMail/src/app/app.component.ts
+++ 03-DojoMail/DojoMail/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  userlist = [
+    {email: 'BILL@gates.com', importance: true, subject: 'New Windows', content: 'Windows XI will launch in year 2100.'},
+    {email: 'ADA@lovelace.com', importance: true, subject: 'Programming', content: 'Enchatress of Numbers'},
+    {email: 'JOHN@carmac.com', importance: false, subject: 'Updated Algo', content: 'New algorithm for shadow volumes.'},
+    {email: 'GABE@newel.com', importance: false, subject: 'HL3!', content: 'Just kdding...'}
+  ];
 }
