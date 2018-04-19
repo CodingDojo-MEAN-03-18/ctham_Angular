45f3014908a762c7ac4bde272abe837225a534d5
Chia T <tham_oh@hotmail.com>
2018-04-18 21:48:45 -0500

    add angular, retro barcode generator

---
+++ 04-RetroBarcodeGenerator/RetroBarcodeGenerator/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Retro Barcode Generator';
+  colour = randColor();
+
+  arrayColor: string[] = arrayColor();
+
+}
+
+function randColor() {
+
+  const colour = 'rgb(' +
+    (Math.floor(Math.random() * 255)) + ',' +
+    (Math.floor(Math.random() * 255)) + ',' +
+    (Math.floor(Math.random() * 255)) + ')';
+
+  return colour;
+}
+
+function arrayColor(): string[] {
+
+  const tempColor: string[] = [];
+
+  for (let x = 0; x < 20; x++) {
+    tempColor.push(randColor());
+  }
+
+  return tempColor;
+}
