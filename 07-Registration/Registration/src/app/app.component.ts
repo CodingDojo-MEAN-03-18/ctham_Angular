e5144d0c3cc3bb004f3aec2ea114ce210cacc860
Chia T <tham_oh@hotmail.com>
2018-04-25 00:39:11 -0500

    angular bug fixed, registration

--- 07-Registration/Registration/src/app/app.component.ts
+++ 07-Registration/Registration/src/app/app.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'Registration';
-  user = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    password2: '',
-    address: '',
-    unit: '',
-    city: '',
-    state: '',
-    lucky: ''
-  };
+
+  user: User = new User();
+  users: Array<User> = [];
+
+  onSubmit(event: Event, userForm: NgForm) {
+    console.log('****', userForm.value);
+    event.preventDefault();
+    const { value, valid } = userForm;
+    console.log(this.user);
+
+    this.users.push(this.user);
+    this.user = new User();
+    userForm.reset();
+  }
 }
--- 07-Registration/Registration/src/app/app.module.ts
+++ 07-Registration/Registration/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule, // <-- Include module in our AppModules
+    FormsModule // <-- Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
